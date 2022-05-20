b2594a416cd524590632c2935166be122d7cca51
EC2 Default User <ec2-user@ip-10-0-0-79.ec2.internal>
2022-05-20 11:36:38 +0000

    week 13 excersise

---
+++ welcome message.py
@@ -0,0 +1,11 @@
+"""
+Your module description
+"""
+firstname = input(str("what is your first name: "))# allow user to input firstname
+
+lastname = input(str("what is your last name: "))# allow user to input last name
+
+fname = firstname # allow user to assign names to variable
+lname = lastname
+
+print("welcome", fname, lname) # print out welcome message using assignes name variable 
