822d34833849654091311154add1c30ab47af6c3
EC2 Default User <ec2-user@ip-10-0-0-79.ec2.internal>
2022-05-20 11:58:47 +0000

    week 13 excersise

---
+++ sq ft.py
@@ -0,0 +1,10 @@
+"""
+Your module description
+"""
+Length = int(input("What is the Length: "))
+Width = int(input("what is the Width: "))
+
+Sqft = Length * Width
+
+print("that the Square Footage of the room is: ", Sqft)
+
